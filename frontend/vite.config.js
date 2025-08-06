1f26b70b65c0bf41b34ac147be86e4250245ba06
Tomer Novogrodtzki <tomer.novogrodtzki@gmail.com>
2025-08-06 17:10:23 +0300

    feat: add frontend React app

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy /review and /health to your FastAPI
+      '/review': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/health': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+})
